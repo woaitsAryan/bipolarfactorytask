934ce7281ec73421a743f337c60d5b5f9a5eec88
woaitsAryan <yoprince.ab@gmail.com>
2024-02-29 18:58:08 +0530

    fix: database name change

--- backend/initializers/db.py
+++ backend/initializers/db.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = MongoClient(os.getenv('MONGO_URI'))
-mongo_db = client['your_database_name']
+mongo_db = client['bipolar']
 
 flight_collection = mongo_db['flights']
 user_collection = mongo_db['users']
